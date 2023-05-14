0364cd220bf39e17a3926e1c651b82c7157158e9
Pavlo B <zetorama@gmail.com>
2023-05-14 16:39:53 -0700

    🧹 configure tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
   plugins: [react()],
   server: {
     open: true,
   },
   build: {
-    outDir: "build",
+    outDir: '../build',
     sourcemap: true,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
+    environment: 'jsdom',
+    setupFiles: './setupTests',
     mockReset: true,
   },
 })
